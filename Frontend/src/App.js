77033b727554d4836e79f5276ef51c2284cbe529
ThomasKindermann <kindermann@kitconcept.com>
2022-03-17 14:21:12 +0100

    add bootstrap to project

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,14 @@
 import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Header } from "./Components";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
