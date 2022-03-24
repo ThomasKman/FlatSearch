4830d0f380067c3f5b29246a87b89082a9f0df36
ThomasKindermann <kindermann@kitconcept.com>
2022-03-24 14:17:20 +0100

    nav bar initial commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,23 @@
+import React, { useState } from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Header } from "./Components";
 
-function App() {
+const App = () => {
+  const [login, setLogin] = useState(true);
+
+  const clickhanler = () => {
+    setLogin(!login);
+  };
+
   return (
     <div className="App">
-      <Header />
+      <Header login={login} />
+      <p>{login}</p>
+      <button onClick={clickhanler}> LOL press me iks deh</button>
     </div>
   );
-}
+};
 
 export default App;
--- Frontend/src/Components/Header/Navigation/Navigation.jsx
+++ Frontend/src/Components/Header/Navigation/Navigation.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+
+import ramsay from "../../../static/Images/ramsay.jpg";
+
+//anmelden / Registrieren je nach anmelde status
 
 const Navigation = (props) => {
   return (
     <div className="nav-bar">
       <Navbar>
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <div>
+            <Navbar.Brand href="/">Suche</Navbar.Brand>
+          </div>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            {props.login ? (
+              <>
+                <Nav.Link href="/ramsay">
+                  <img className="ramsay" alt="lambsauce" src={ramsay} />
+                </Nav.Link>
+                <Nav.Link href="/logout">Logout</Nav.Link>
+              </>
+            ) : (
+              <Nav.Link href="#home">Anmeldung / Registrieren</Nav.Link>
+            )}
+
+            {/* <Nav.Link href="#features">Anmelden</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
