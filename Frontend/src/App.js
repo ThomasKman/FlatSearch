9ab886ffa6a505d5cc415c01216d8f4ba52d734a
ThomasKindermann <kindermann@kitconcept.com>
2022-04-07 13:10:36 +0200

    add react router to the project

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,11 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Header, View } from "./Components";
 
+import { Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <View view="search" />
+      <Routes>
+        <Route path="/" element={<View view="main" />} />
+        <Route path="search" element={<View view="search" />} />
+      </Routes>
     </div>
   );
 }
